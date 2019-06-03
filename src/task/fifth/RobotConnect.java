ad759d5552b3daf20475f1e0f78218952d87e0c2
Maryana <renvema@bigmir.net>
2019-06-03 09:50:54 +0300

    les 5 commit2

---
+++ src/task/fifth/RobotConnect.java
@@ -0,0 +1,48 @@
+package task.fifth;
+
+public class RobotConnect {
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        for (int i = 0; i < 3; i++) {
+            RobotConnection connection = null;
+            try {
+                connection = robotConnectionManager.getConnection();
+                connection.moveRobotTo(toX, toY);
+                i = 3;
+
+            } catch (RobotConnectionException e) {
+
+                if (i == 2) throw e;
+
+            } finally {
+                try {
+                    if (connection != null) connection.close();
+                } catch (RobotConnectionException e) {
+
+                }
+            }
+        }
+    }
+
+    public interface RobotConnection extends AutoCloseable {
+        void moveRobotTo(int x, int y);
+
+        @Override
+        void close();
+    }
+
+    public interface RobotConnectionManager {
+        RobotConnection getConnection();
+    }
+
+    public class RobotConnectionException extends RuntimeException {
+
+        public RobotConnectionException(String message) {
+            super(message);
+
+        }
+
+        public RobotConnectionException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
